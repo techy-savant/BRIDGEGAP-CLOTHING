c95db8eb32a8a7857f7f0e0fdff87d0fc38bd01a
Code bee <codebee345@outlook.com>
2023-09-13 23:07:54 +0100

    added user authentication and password reset

--- assets/js/authforms.js
+++ assets/js/authforms.js
@@ -1,12 +1,20 @@
 const signupForm = document.getElementById('signup-form')
 const loginform = document.getElementById('login-form')
+const resendActivatoinForm = document.getElementById('resend-activation-form')
+const requestResetPasswordForm = document.getElementById('request-reset-password-form')
+const resetPasswordForm = document.getElementById('reset-password-form')
 const alertActionMessage = document.querySelector('.action-message')
+
+const queryString = window.location.search;
+const queryParams = new URLSearchParams(queryString)
 
 const domain = localStorage.getItem('domain')
 let readyToLogin = false
 let returnData
 let redirect = false
 
+
+//user is trying to signup
 if(signupForm !== null){
     signupForm.addEventListener('submit', function(e){
         e.preventDefault()
@@ -16,6 +24,7 @@ if(signupForm !== null){
         const endpoint = domain +'/api/signup/'
     
         let formData = new FormData(this)
+        let requestOk = false
         fetch(endpoint, {
             method: 'POST',
             body: formData,
@@ -30,14 +39,20 @@ if(signupForm !== null){
                 
             }
             else{
-                readyToLogin = true
+                requestOk = true
             }
             return response.json()
         })
         .then(data =>{
+            if(requestOk){
+                showCoverScreen(data.email)
+            }
+            else{
             data.message == undefined? alertActionMessage.textContent = 'Fatal: an error occured': alertActionMessage.textContent = data.message
             transitionModal('action-msg-alert-modal')
             console.log(data)
+            }
+            
         })
         .catch(error =>{
             alertActionMessage.textContent = 'Something went wrong please try again'
@@ -48,7 +63,45 @@ if(signupForm !== null){
     
         
     })
-    
+}
+
+
+function showCoverScreen(email){
+    transitionModal('none')
+    const coverScreen = document.querySelector('.cover-screen')
+    const userEmailForConfirmation = document.getElementById('user-email-for-confirmation')
+
+    userEmailForConfirmation.textContent = email
+
+    coverScreen.style.display = 'flex'
+
+    const reqeustSpan = document.getElementById('request-span')
+    reqeustSpan.addEventListener('click', function(e){
+        resendActivationEmail(email)
+
+    })
+}
+
+function resendActivationEmail(email){
+    transitionModal('loading-modal')
+        //resend confirmation mail
+        let resendConfEndpoint = domain + `/api/resendactivationemail?email=${email}`
+
+        fetch(resendConfEndpoint, {
+            method: 'POST'
+        })
+        .then(response =>{
+            if(!response.ok){
+                console.log('Error resending email')
+            }
+            
+            return response.json()
+        })
+        .then(data =>{
+            data.message == undefined? alertActionMessage.textContent = 'Fatal: an error occured': alertActionMessage.textContent = data.message
+            transitionModal('action-msg-alert-modal')
+            console.log(data)
+        })
 }
 
 function getCookie(name) {
@@ -57,6 +110,7 @@ function getCookie(name) {
     if (parts.length === 2) return parts.pop().split(';').shift();
 }
 
+//user is trying to login
 if(loginform !== null){
     loginform.addEventListener('submit', function(e){
         e.preventDefault()
@@ -94,7 +148,81 @@ if(loginform !== null){
     })
 }
 
+//user is trying to get an activation email
+if(resendActivatoinForm !== null){
+    resendActivatoinForm.addEventListener('submit', function(e){
+        e.preventDefault()
+        transitionModal('none')
+        const emailInput = document.getElementById('resend-email-input')
+        resendEmail = emailInput.value
+        resendActivationEmail(resendEmail)
 
+    })
+}
+
+
+//user is trying to get a password reset link
+if(requestResetPasswordForm !== null){
+    requestResetPasswordForm.addEventListener('submit', function(e){
+        e.preventDefault()
+        transitionModal('loading-modal')
+        let formData = new FormData(this)
+        let requestPasswordResetEndpoint = domain + '/api/requestresetpassword/'
+
+        fetch(requestPasswordResetEndpoint, {
+            method: 'POST',
+            body: formData
+        }).then(response => {
+            if(!response.ok){
+                console.log('error reqeusting reset password')
+            }
+            return response.json()
+        }).then(data => {
+            data.message == undefined? alertActionMessage.textContent = 'Fatal: an error occured': alertActionMessage.textContent = data.message
+            transitionModal('action-msg-alert-modal')
+            console.log(data)
+        }).catch(error => {
+            console.error('Fetch error: ', error)
+        })
+    })
+}
+
+//user is trying to reset thier password
+if(resetPasswordForm !== null){
+    resetPasswordForm.addEventListener('submit', function(e){
+        e.preventDefault()
+        transitionModal('loading-modal')
+
+        let formData = new FormData(this)
+       
+        const uidb64 = queryParams.get('uidb64')
+        const token = queryParams.get('token')
+        const resetpasswordEndpoint = domain + `/api/changeuserpassword?uidb64=${uidb64}&token=${token}`
+
+        fetch(resetpasswordEndpoint, {
+            method: 'POST', 
+            body: formData
+        })
+        .then(response =>{
+            if(!response.ok){
+                console.log('Error changing user password')
+            }
+            else{
+                readyToLogin= true
+            }
+            return response.json()
+        })
+        .then(data => {
+            data.message == undefined? alertActionMessage.textContent = 'Fatal: an error occured': alertActionMessage.textContent = data.message
+            transitionModal('action-msg-alert-modal')
+        })
+        .catch(error => {
+            console.error('Fetch error: ', error)
+        })
+
+
+    })
+}
 
 function transitionModal(className){
     const modals = document.querySelectorAll('.modal-box')
---
+++ assets/js/verifyemail.js
@@ -0,0 +1,43 @@
+const loadingInitial= document.querySelector('.loading-initial')
+
+const queryString = window.location.search;
+const queryParams = new URLSearchParams(queryString)
+
+const uidb64 = queryParams.get('uidb64')
+const token = queryParams.get('token')
+
+const domain = localStorage.getItem('domain')
+
+const validateEndpoint = domain + `/api/validateuser?uidb64=${uidb64}&token=${token}`
+
+fetch(validateEndpoint, {
+    method: 'POST'
+})
+.then(response =>{
+    if(!response.ok){
+        removeInitalLoading()
+        showSection('verf-failed-section')
+    }
+    else{
+        removeInitalLoading()
+        showSection('verf-success-section')
+    }
+})
+
+
+
+function removeInitalLoading(){
+    loadingInitial.style.display = 'none'
+}
+
+
+function showSection(elementId){
+    const section = document.getElementById(elementId)
+    
+    const allsections = document.querySelectorAll('.acct-con')
+    allsections.forEach(function(section){
+        section.style.display= 'none'
+    })
+
+    section.style.display = 'flex'
+}
